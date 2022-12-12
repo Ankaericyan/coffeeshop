5e6df2ebe6c283706b085e7a30b873618e2acb57
Ankaericyan <79460134+Ankaericyan@users.noreply.github.com>
2022-12-13 01:19:18 +0400

    Revert "end project"

--- src/app/pages/clients-feedback/clients-feedback.component.ts
+++ src/app/pages/clients-feedback/clients-feedback.component.ts
@@ -1,15 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clients-feedback',
   templateUrl: './clients-feedback.component.html',
   styleUrls: ['./clients-feedback.component.css']
 })
-export class ClientsFeedbackComponent implements OnInit {
-  @Input() slideFor!: string;
-  public images: any = [];
-  constructor() {}
-  ngOnInit(): void {
+export class ClientsFeedbackComponent {
 
-  }
 }
